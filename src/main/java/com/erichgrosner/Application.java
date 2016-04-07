586a2af1ecdc87954bdac48a142a28c1538bdcd8
egrosner <egrosner@gmail.com>
2016-04-07 00:46:16 -0400

    website now deployed as a WAR to aws

--- src/main/java/com/erichgrosner/Application.java
+++ src/main/java/com/erichgrosner/Application.java
@@ -2,9 +2,18 @@ package com.erichgrosner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
 
